60fbeafccdd7cb3d1429742c3a8907819b177d56
Ganesh <ganuofficial2795@gmail.com>
2021-08-14 13:01:42 +0530

    Tasks added

--- todo.js
+++ todo.js
@@ -1,1 +1,38 @@
 console.log("TODO-APP 🔥")
+
+let task = document.querySelector("#task");
+let desc = document.querySelector("#desc");
+let btn = document.getElementById("btn");
+let box = document.querySelector("#box");
+// console.log(box)
+
+btn.addEventListener("click",function(){
+    
+    
+    if(task.value==="" || desc.value===""){
+        alert("Property is missing")
+    }else{
+        console.log("Got your task")
+        show_tasks()
+        task.value = ""
+        desc.value = ""
+    }
+
+});
+
+function show_tasks(){
+
+    let element = document.createElement("div");
+    let html = ""
+    html+=`
+    <div class="list-group-item mb-2">
+    <label class="list-group pl-2" style="display: inline-block;">
+        <input class="form-check-input me-1 p-2" type="checkbox">${task.value}
+    </label>
+    </div>`
+    
+    element.innerHTML = html 
+    box.append(element)
+};
+
+
