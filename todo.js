34ec0f457680350611d55f6eca1cf92f867a98c6
Ganesh <ganuofficial2795@gmail.com>
2021-08-15 16:36:18 +0530

    Task completed

--- todo.js
+++ todo.js
@@ -17,22 +17,20 @@ btn.addEventListener("click",function(){
         show_tasks()
         task.value = ""
         desc.value = ""
-        
-    }
-
-});
+        delete_todo()
+}});
 
 function show_tasks(){
 
     let element = document.createElement("div");
     let html = "";
     html+=`
-    <div class="card text-white bg-dark mb-3 mt-4 mx-3 my-4" style="width: 10rem; text-align:center;">
+    <div class="card text-white bg-dark mb-3 mt-4 mx-3 my-4" style="width: 10rem; text-align:center; border-radius:15;">
     <div class="card-header">${task.value}</div>
     <div class="card-body">
         <p class="card-text">${desc.value}</p> 
     </div>
-    <button type="button" class="btn btn-danger" style="margin:1rem;">Delete</button>
+    <button type="button" class="btn btn-danger completed" style="margin:1rem;">Finish</button>
     </div>`
     
     element.innerHTML = html 
@@ -40,7 +38,23 @@ function show_tasks(){
 
 };
 
+function delete_todo(){
 
+    let all_delete_buttons = document.getElementsByClassName("completed");
+    // console.log(all_delete_buttons)
+    Array.from(all_delete_buttons).forEach(function(e){
+
+        e.addEventListener("click",function(){
+        
+            e.setAttribute('style',"margin:1rem; background-color:green; color:white; text-align:center; padding:3px; border-radius:3px;")
+            e.setAttribute('class',"disabled")
+            e.innerHTML = "Task Completed"
+
+        })
+    })
+    
+    
+};
 
 
 
